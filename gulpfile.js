669dc279168baa6b2e42ae7d55c2c9d01591ca64
Andrey Ponomarenko <slaner2008@yandex.ru>
2019-04-30 19:05:20 +0500

    add watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,23 @@
 'use strict';
 
 const gulp = require('gulp');
+const { watch } = gulp;
 const sass = require('gulp-sass');
 const cssmin = require('gulp-cssmin');
 const rename = require('gulp-rename');
 const env = require('yargs').argv;
 const gulpif = require('gulp-if');
 
-gulp.task('css', function () {
-	const build = env.build;
-	const willMinify = env.min;
-	return gulp.src(`./docs/src/css/${build}/main.scss`)
-		.pipe(sass.sync().on('error', sass.logError))
-		.pipe(gulpif(willMinify, cssmin()))
-		.pipe(rename(`${build}.bundle.css`))
-		.pipe(gulp.dest(`./docs/dist/css`));
+gulp.task('css:w', function() {
+    watch('docs/src/css/**/*.scss', {}, gulp.series('css'))
+});
+
+gulp.task('css', function() {
+    const build = env.build;
+    const willMinify = env.min;
+    return gulp.src(`./docs/src/css/${build}/main.scss`)
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(gulpif(willMinify, cssmin()))
+        .pipe(rename(`${build}.bundle.css`))
+        .pipe(gulp.dest(`./docs/dist/css`));
 });
